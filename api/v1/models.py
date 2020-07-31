47fed20869794cc198f049a5d194649113d1a05b
Alexey Tulovskiy <tulov.alex@gmail.com>
2020-07-31 08:16:31 +0000

    Created models

--- api/v1/models.py
+++ api/v1/models.py
@@ -1,3 +1,31 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Program(models.Model):
+    min_credit = models.IntegerField(null=False, blank=False)
+    max_credit = models.IntegerField(null=False, blank=False)
+    min_age_borrower = models.IntegerField(null=False, blank=False)
+    max_age_borrower = models.IntegerField(null=False, blank=False)
+
+
+class Borrower(models.Model):
+    iin = models.CharField(max_length=12, null=False, blank=False)
+    birthday = models.DateField(null=False)
+
+
+class Order(models.Model):
+    program = models.ForeignKey(Program,
+                                related_name='orders',
+                                on_delete=models.CASCADE)
+    borrower = models.ForeignKey(Borrower,
+                                 related_name='orders',
+                                 on_delete=models.CASCADE)
+    sum = models.IntegerField(null=False, blank=True)
+    status = models.CharField(choices=[('approved', 'одобрено'),
+                                       ('rejected', "отказ")],
+                              null=True, max_length=12)
+    rejection_reason = models.TextField(max_length=1000, null=True)
+
+
+class BlackList(models.Model):
+    iin = models.CharField(max_length=12, db_index=True)
